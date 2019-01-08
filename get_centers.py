6c7c6f6f98c96e9e7d1b62c968967c80d73527f9
Hesher Ya <haizhou.ya@mail.com>
2019-01-08 10:55:58 +0800

    增加样本图片，get_centers 去掉阈值转换、模糊及形态变换，参数化窗口高度

--- get_centers.py
+++ get_centers.py
@@ -1,12 +1,9 @@
-import sys
 import time
 
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.cluster import MeanShift, estimate_bandwidth
-
-WINDOW_HEIGHT = 10
 
 
 def get_nonzero(image):
@@ -17,14 +14,14 @@ def get_nonzero(image):
     return np.stack((nonzero_x, nonzero_y), axis=-1)
 
 
-def mean_shift(img, src, line):
+def mean_shift(img, src, line, window_height):
     """聚类并在结果数组中插入行号"""
     bandwidth = estimate_bandwidth(src, quantile=0.2, n_samples=500)
 
     ms = MeanShift(bandwidth=bandwidth, bin_seeding=True, cluster_all=False)
     ms.fit(src)
 
-    labels = ms.labels_
+    # labels = ms.labels_
     cluster_centers = ms.cluster_centers_
 
     h = img.shape[0]
@@ -36,62 +33,61 @@ def mean_shift(img, src, line):
     centers = np.insert(cluster_centers, 0, values=int(line), axis=1)
 
     for pot in centers:
-        pot[2] = h - (line + 1) * WINDOW_HEIGHT + pot[2]
+        pot[2] = h - (line + 1) * window_height + pot[2]
 
     return centers.astype(np.int32)
 
 
-def get_centers(image):
+def get_centers(image, ms_window_height):
     """按行对图片聚类并返回三维数组"""
     h = image.shape[0]
 
-    # 高斯模糊
-    blur_img = cv2.GaussianBlur(image, (5, 5), 0)
+    # # 高斯模糊
+    # blur_img = cv2.GaussianBlur(image, (5, 5), 0)
+    #
+    # # 阈值转换
+    # ret, threshold_img = cv2.threshold(blur_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    #
+    # # 形态转换（过滤噪点）
+    # kernel = np.ones((5, 5), np.uint8)
+    # opening_img = cv2.morphologyEx(threshold_img, cv2.MORPH_OPEN, kernel)
 
-    # 阈值转换
-    ret, threshold_img = cv2.threshold(blur_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-    # 形态转换（过滤噪点）
-    kernel = np.ones((5, 5), np.uint8)
-    opening_img = cv2.morphologyEx(threshold_img, cv2.MORPH_OPEN, kernel)
+    opening_img = image
 
     start_time = time.time()
     count = 0
     results = []
-    while h - WINDOW_HEIGHT > 0:
-        split_img = opening_img[h - WINDOW_HEIGHT: h, ]
+    while h - ms_window_height > 0:
+        split_img = opening_img[h - ms_window_height: h, ]
         nonzero = get_nonzero(split_img)
         if nonzero.size > 0:
-            points = mean_shift(image, nonzero, count)
+            points = mean_shift(image, nonzero, count, ms_window_height)
             results.append(points.tolist())
-        h -= WINDOW_HEIGHT
+        h -= ms_window_height
         count += 1
     end_time = time.time()
 
-    print("image shape", opening_img.shape, "窗口高度", WINDOW_HEIGHT, "循环次数", count, "执行时间", end_time - start_time, "s")
+    print("image shape", opening_img.shape, "窗口高度", ms_window_height, "循环次数", count, "执行时间", end_time - start_time, "s")
 
     return results, opening_img
 
 
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv
-
-    img = cv2.imread('res/img/3.png', cv2.IMREAD_GRAYSCALE)
+def main():
+    img = cv2.imread('res/img/c6_1.jpg', cv2.IMREAD_GRAYSCALE)
 
     # 输出图形及聚类后的点
     plt.subplot(211)
     plt.imshow(img, cmap='gray')
     plt.subplot(212)
 
-    points, filter_img = get_centers(img)
+    points, filter_img = get_centers(img, 30)
     print("------->\n", points)
     for p in points:
         for po in p:
             cv2.circle(filter_img,
                        (int(po[1]), int(po[2])), 5, (111, 111, 111), -1)
 
-    plt.imshow(filter_img, cmap='gray')
+    plt.imshow(filter_img)
     plt.show()
 
 
